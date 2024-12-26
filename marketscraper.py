a8719be737110935434325ad88e6b2a252ef3e98
redddddsu <yredsu@gmail.com>
2024-12-26 17:01:06 +0800

    Add the ability to select other data center (NA ONLY)

--- marketscraper.py
+++ marketscraper.py
@@ -5,6 +5,29 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
+print("Press the number of your data center")
+
+dataCenters = ["Aether", "Crystal", "Dynamis", "Primal"]
+
+for i in range(len(dataCenters)):
+    print(str(i + 1) + ". " + dataCenters[i])
+
+
+whichCenter = input()
+
+if (whichCenter == "1"):
+    whichCenter = '//*[@value="Siren"]'
+elif (whichCenter == "2"):
+    whichCenter = '//*[@value="Coeurl"]'
+elif (whichCenter == "3"):
+    whichCenter = '//*[@value="Golem"]'
+elif (whichCenter == "4"):
+    whichCenter = '//*[@value="Famfrit"]'
+else:
+    print("Invalid number, default to Siren")
+    whichCenter = '//*[@value="Siren"]'
+
+
 service = Service(executable_path="chromedriver.exe")
 driver= webdriver.Chrome(service=service)
 
@@ -25,10 +48,10 @@ for i in range(6):
         selectServer = driver.find_element(By.CLASS_NAME, "servers").click()
 
         WebDriverWait(driver, 5).until (
-            EC.presence_of_all_elements_located((By.XPATH, '//*[@value="Siren"]'))
+            EC.presence_of_all_elements_located((By.XPATH, whichCenter))
         )
 
-        selectServer = driver.find_element(By.XPATH, '//*[@value="Siren"]').click()
+        selectServer = driver.find_element(By.XPATH, whichCenter).click()
         selectServer = driver.find_element(By.CLASS_NAME, "btn-green").click()
 
     getPrice = driver.find_elements(By.CLASS_NAME, "price-current")
